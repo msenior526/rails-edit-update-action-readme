aa2b887391a9a3e0baab23355cb4973a4c5d89fb
msenior526 <msenior526@gmail.com>
2021-03-01 13:24:25 -0500

    commit tree 29d5f398d3a7d08292e7c1b0f3b17bb48c96d82a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create(title: "GLASSESUSA",description: "Have amazing glasses at an affordable price!")
+Article.create(title: "Nintendo",description: "The Nintendo Direct released about 2 weeks ago.")
+Article.create(title: "Kia",description: "My neck, my back.")
+Article.create(title: "Andrew Cuomo",description: "Gov. Cuomo is accused of sexual harrassment in the workplace.")
+Article.create(title: "Locke & Key",description: "I heard season 2 is coming out soon.")
